a7220bca34385113d559f7344d128821987ebab0
dennis-grinwald <grinwald.dennis@gmail.com>
2025-01-08 17:23:56 +0100

    update

--- test.py
+++ test.py
@@ -22,13 +22,13 @@ task_vectors = [
 # Test loop
 task_accs = {}
 for i, weight in enumerate(np.arange(0.0,1.1,0.1)):
-    merged_task_vector = copy.deepcopy(weighted_sum(task_vectors, [0.5, 0.5]))
+    alpha_vector = weighted_sum(task_vectors, [0.5, 0.5])
     tmp_alphas = [weight, 1 - weight]
-    merged_task_vector_alpha = weighted_sum(task_vectors, tmp_alphas)
-
+    merged_task_vector = weighted_sum(task_vectors, tmp_alphas)
     # Exchange weights of the last linear layer
-    merged_task_vector.vector['model.visual.transformer.resblocks.23.mlp.c_proj.weight'] = merged_task_vector_alpha.vector['model.visual.transformer.resblocks.23.mlp.c_proj.weight']
-    merged_task_vector.vector['model.visual.transformer.resblocks.23.mlp.c_proj.bias'] = merged_task_vector_alpha.vector['model.visual.transformer.resblocks.23.mlp.c_proj.bias']
+    merged_task_vector.vector['model.visual.transformer.resblocks.1.mlp.c_proj.weight'] = alpha_vector.vector['model.visual.transformer.resblocks.1.mlp.c_proj.weight']
+    merged_task_vector.vector['model.visual.transformer.resblocks.1.mlp.c_proj.bias'] = alpha_vector.vector['model.visual.transformer.resblocks.1.mlp.c_proj.bias']
+
     # Apply the resulting task vector
     image_encoder = merged_task_vector.apply_to(pretrained_checkpoint, scaling_coef=1.0)
     # Evaluate
